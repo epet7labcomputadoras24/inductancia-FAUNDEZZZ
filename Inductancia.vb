771390c517885abb1e846724ad4094890b5ee9c7
FAUNDEZZZ <lautaroalejandro11xd@gmail.com>
2024-08-06 19:30:46 -0300

    Tarea entregada faundez

---
+++ Inductancia.vb
@@ -0,0 +1,23 @@
+﻿Imports System
+Module Program
+    Dim frecuencia As Double
+    Dim inductancia As Double
+    Dim XL As Double
+    Dim PI = 3.1416
+    Sub Main(args As String())
+        Console.WriteLine("INGRESE VALOR DE FRECUENCIA")
+        frecuencia = Console.ReadLine()
+        Console.WriteLine("INGRESE VALOR DE INDUCTANCIA")
+        inductancia = Console.ReadLine()
+        XL = 2 * PI * frecuencia * inductancia
+        Console.WriteLine("valor de XL" & XL)
+
+
+    End Sub
+End Module
+
+Imports Microsoft.VisualBasic
+
+Public Class Class1
+
+End Class
